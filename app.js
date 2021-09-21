45d0818329237949d348be311097b43c173a5d19
Matheus Filipe <matheusfilipebb@gmail.com>
2021-09-21 02:15:51 -0300

    layout melhor e codigo limpo

--- app.js
+++ app.js
@@ -43,12 +43,12 @@ function appendItem(obj,index) {
   //edit BUTTON
   const editButton = document.createElement("button")
   editButton.innerHTML = '<i class="bi-pencil-fill"></i>'
-  editButton.classList.add("edit-btn")
+  editButton.className = ("edit-btn btn btn-sm px-1 py-0")
   buttons.appendChild(editButton)
   //trash BUTTON
   const trashButton = document.createElement("button")
   trashButton.innerHTML = '<i class="bi-trash-fill"></i>'
-  trashButton.classList.add("trash-btn")
+  trashButton.className = ("trash-btn btn btn-sm px-1 py-0")
   buttons.appendChild(trashButton)
   itemTr.appendChild(buttons)
   //append to LIST
@@ -81,8 +81,7 @@ function addItem(evt) {
 }
 
 //botões DELETE e EDIT
-let valuePredio = "",
-  valueLocal = ""
+let valueLocal = ""
 function deleteEdit(evt) {
   let arrLocaisTrabalho = JSON.parse(sessionStorage.getItem("arrLocaisTrabalho"))
   const item = evt.target
@@ -91,24 +90,24 @@ function deleteEdit(evt) {
   if (item.classList[0] === "trash-btn") {
     arrLocaisTrabalho = arrLocaisTrabalho.filter((obj,index) => index != item.parentElement.parentElement.id)
     sessionStorage.setItem("arrLocaisTrabalho", JSON.stringify(arrLocaisTrabalho))
-    item.parentElement.parentElement.remove()
-    //regarrega a lista para reordenar os IDs de cada TR
+    //regarrega a LISTA e reordena os IDs de cada TR
     showList()
   }
 
-  //Formulário EDIT
+  //"Formulário" EDIT
   if (item.classList[0] === "edit-btn") {
-    valuePredio = item.parentElement.parentElement.querySelectorAll("td")[0].innerText.slice(7)
+    const valuePredio = item.parentElement.parentElement.querySelectorAll("td")[0].innerText.slice(7)
     valueLocal = item.parentElement.parentElement.querySelectorAll("td")[1].innerText
     let options = ""
     for (let i = 1; i <= 5; i++) {
       let isSelected = valuePredio == i ? "selected" : ""
       options += `<option ${isSelected} value="${i}">Prédio ${i}</option>`
     }
+    //insere o "formulário" de edição em cada TD
     item.parentElement.parentElement.querySelectorAll("td")[0].innerHTML = `<select class="form-select">${options}`
     item.parentElement.parentElement.querySelectorAll("td")[1].innerHTML = `<input type="text" class="form-control" value="${valueLocal}"}/>`
     item.parentElement.parentElement.querySelectorAll("td")[2].innerHTML =
-      '<button class="check-btn"><i class="bi-check-lg"></i></button><button class="x-btn"><i class="bi-x-lg"></i></button>'
+      '<button class="check-btn btn btn-sm px-1 py-2"><i class="bi-check-lg"></i></button><button class="x-btn btn btn-sm px-1 py-2"><i class="bi-x-lg"></i></button>'
   }
 
   //Confirm EDIT
@@ -116,21 +115,16 @@ function deleteEdit(evt) {
     const newValuePredio = item.parentElement.parentElement.querySelector("select").value
     const newValueLocal = item.parentElement.parentElement.querySelector("input").value
     const id = item.parentElement.parentElement.id
-
-    arrLocaisTrabalho.splice(id,1,{ predio: newValuePredio, local: newValueLocal ? newValueLocal : valueLocal, id: id })
+    //armazena ITEM em 'arrLocaisTrabalho'
+    arrLocaisTrabalho.splice( id, 1, { predio: newValuePredio, local: newValueLocal ? newValueLocal : valueLocal, id: id })
     sessionStorage.setItem("arrLocaisTrabalho", JSON.stringify(arrLocaisTrabalho))
-    
-    item.parentElement.parentElement.querySelectorAll("td")[0].innerHTML = `Prédio ${newValuePredio}`
-    item.parentElement.parentElement.querySelectorAll("td")[1].innerHTML = newValueLocal ? newValueLocal : valueLocal
-    item.parentElement.parentElement.querySelectorAll("td")[2].innerHTML =
-      '<button class="edit-btn"><i class="bi-pencil-fill"></i></button><button class="trash-btn"><i class="bi-trash-fill"></i></button>'
+    //recarrega a LISTA
+    showList()  
   }
 
   //Cancel EDIT
   if (item.classList[0] === "x-btn") {
-    item.parentElement.parentElement.querySelectorAll("td")[0].innerHTML = `Prédio ${valuePredio}`
-    item.parentElement.parentElement.querySelectorAll("td")[1].innerHTML = valueLocal
-    item.parentElement.parentElement.querySelectorAll("td")[2].innerHTML =
-      '<button class="edit-btn"><i class="bi-pencil-fill"></i></button><button class="trash-btn"><i class="bi-trash-fill"></i></button>'
+    //recarrega a LISTA
+    showList()  
   }
 }
